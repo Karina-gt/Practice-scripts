0ef34af3e27bd06d55113df71d55002c82e0e60b
Karina <you@example.com>
2025-06-18 16:37:47 +0000

    lottery

---
+++ lottery.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "******Welcometo lotterycheck vibhag********"
+
+read -p "Enter your lottery num bro:" num
+if [ $num == "454545" ];
+then
+echo "Yooooo broooo!bhadaiya bhadaiya apko milta hai 7 crore"
+else
+echo "Bhai tujhsena ho paii"
+fi
