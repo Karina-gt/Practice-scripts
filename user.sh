2dec995da4fd67a950f7ca44303fe671d0604d1a
Karina <Karina>
2025-06-18 15:57:43 +0000

    user

---
+++ user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter your good name:" name
+
+echo "OHH  nice name:$name"
+
+read -p "Suffering from any trauma:" trauma
+
+echo $trauma
+
+read -p "Wanna build a life:" life
+
+sleep 3
+
+echo "then join Rishabh sir's class sab thik hjaega si"
